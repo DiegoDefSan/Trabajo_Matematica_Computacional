c3658c22ec4e06e726966d7d53cd36a0d1ccdda3
Diego Defilippi Santillan <defsan35@gmail.com>
2023-04-23 07:46:05 -0500

    Actualizacion-ecualizacion

--- App_button.js
+++ App_button.js
@@ -41,25 +41,19 @@ function getPixeles(img, length, arrIntensities) {
         
         arrIntensities[intensity]++;
     }
-
-    // [12, 24, 10, 0, ..., 89]
-
-    for (let i=0; i<length; i++) {
-        console.log("Intensidad: "+i+": "+arrIntensities[i]);
-    }
 }
 
-function constructBarChart(arrIntensities, length) {
+function constructBarChart(arrIntensities, length, idContainer, idCanvas) {
     // Creo elemento html para colocar para colocar mi chart
-    document.getElementById("container-canvas").innerHTML="";
+    document.getElementById(idContainer).innerHTML="";
     var canvas = document.createElement("canvas");
-    canvas.id="myChart";
+    canvas.id=idCanvas;
 
     // Inserto contexto
-    document.getElementById("container-canvas").appendChild(canvas);
+    document.getElementById(idContainer).appendChild(canvas);
     
     
-    const context = document.getElementById('myChart');
+    const context = document.getElementById(idCanvas);
     
     // Creo indices de X
     let indexes = [];
@@ -94,13 +88,79 @@ function constructBarChart(arrIntensities, length) {
 
 }
 
+
+function editImageEcualizacion(img, intensidades, length) {
+    // // Creacion del canvas
+    let newCanvas = document.createElement('canvas');
+    newCanvas.width = img.width;
+    newCanvas.height = img.height;
+
+    // Asignar tamaño máximo a la imagen
+    newCanvas.style.height="250px";
+
+    // Obtener el contexto 2D del canvas
+    const ctx = newCanvas.getContext('2d');
+
+    // Dibujar la imagen en el canvas
+    ctx.drawImage(img, 0, 0);
+
+    // Obtener los valores de los píxeles de la imagen
+    const imageData = ctx.getImageData(0, 0, newCanvas.width, newCanvas.height);
+    const pixels = imageData.data;
+
+    // Obtener la frecuencia acumulada por intensidades
+        // intensidades = [2, 5, 10, 3, 6]
+        // frecuenciaAcumulada = [2, 7, 17, 20, 26]
+    let frecuenciaAcumulada = new Array(length).fill(0);
+    frecuenciaAcumulada[0] = intensidades[0];
+    for (let i = 1; i < 256; i++) {
+        frecuenciaAcumulada[i] = frecuenciaAcumulada[i - 1] + intensidades[i];
+    }
+
+    // Declaramos arreglo para almacenar los nuevos valores de pixeles
+    let newIntensidades=new Array(length).fill(0);
+
+    for (let i = 0; i < pixels.length; i += 4) {
+        // Se obtiene la intensidad del pixel
+        let r = pixels[i];
+        let g = pixels[i + 1];
+        let b = pixels[i + 2];
+        let intensity = Math.round((r + g + b) / 3);
+
+        // Se obtiene la nueva intensidad a partir de la frecuencia acumulada y la ecuación ((L-1)/M*N)(Hi)
+        let newIntensity=Math.round(((255)/(newCanvas.width*newCanvas.height))*(frecuenciaAcumulada[intensity])); 
+
+        // Se le asigna las nuevas intensidades;
+        pixels[i] = newIntensity;
+        pixels[i + 1] = newIntensity;
+        pixels[i + 2] = newIntensity;
+
+        newIntensidades[newIntensity]++;
+    }
+
+    // Se construye el gráfico con las nuevas intensidades
+    constructBarChart(newIntensidades, length, "new-container-canvas", "myNewChart");
+
+    // Establecer los nuevos valores de los píxeles en la imagen
+    ctx.putImageData(imageData, 0, 0);
+
+    // Mostrar la imagen en la página
+    document.getElementById('updated-img').innerHTML="";
+    document.getElementById('updated-img').appendChild(newCanvas);
+    
+}
+
+
 function transformImage() {
     let img = new Image();
     img.src=document.getElementById("imagen").src;
     let length=256; // Rango de pixeles [0:255]
     let intensidades = new Array(length).fill(0); // [0,0,0, ... , 0, 0]
 
-    getPixeles(img, length, intensidades);
-    constructBarChart(intensidades, length)
-}
 
+    getPixeles(img, length, intensidades);
+    constructBarChart(intensidades, length, "container-canvas", "myChart");
+
+    // Ecualización
+    editImageEcualizacion(img, intensidades, length);
+}
